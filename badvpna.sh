33e6d31ddb251c03467b731dec7eb0ba2fa0f9a4
axangah <axangah@gmail.com>
2017-12-17 12:35:22 +0800

    Update badvpna.sh

--- badvpna.sh
+++ badvpna.sh
@@ -1,1 +1,1 @@
-cd /usr/bin && wget https://github.com/axangah/AUTOSCRIPT/blob/master/badvpn-udpgw && chmod 755 badvpn-udpgw && wget https://github.com/axangah/AUTOSCRIPT/blob/master/rc.local && cp -f rc.local /etc/rc.local && rm -f badvpna.sh && badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/nul &
+cd /usr/bin && wget https://github.com/axangah/AUTOSCRIPT/blob/master/badvpn-udpgw?raw=true && wget https://raw.githubusercontent.com/axangah/AUTOSCRIPT/master/rc.local && cp -f rc.local /etc/rc.local && rm -f badvpna.sh && badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/nul &
